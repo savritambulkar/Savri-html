65ef19de3d94913afbcc6d40ca4ab10953d5692b
savritambulkar <savritambulkar4@gmail.com>
2024-12-15 04:39:54 -0800

    Add files via upload

---
+++ tuet33.js
@@ -0,0 +1,53 @@
+// const parent=document.querySelector('.parent').children
+// console.log(parent)
+// const p=Array.from(parent);
+// console.log(p)
+// p.forEach(e => { 
+//     console.log(e)
+// });
+
+//const parent=document.querySelector('.parent')
+// console.log(parent)
+// console.log(parent.children)
+//console.log(parent.children[1].innerHTML)
+// console.log(parent.firstElementChild)
+// console.log(parent.lastElementChild)
+
+// const child=document.querySelector('.item').parentElement
+// console.log(child)
+// console.log(child.style.border='4px solid black');
+
+// function addlang(langName){
+//     const li=document.createElement('li')
+//     li.innerHTML =`${langName}`
+//    document.querySelector(".lang").appendChild(li)
+  
+// }
+// addlang("java")
+// addlang("Python")
+// addlang("React")
+// addlang("java")
+// addlang("Python")
+// addlang("React")
+
+// function addlanguage(langName){
+//     const li=document.createElement('li')
+//     li.appendChild(document.createTextNode(langName))
+//     document.querySelector('.lang').appendChild(li)
+// }
+// addlanguage("CSS")
+// addlanguage("HTML")
+// //const newone=document.querySelector("li:nth-child(3)").innerHTML="MongoDB"
+// const newone=document.querySelector("li:nth-child(3)")
+// const newli=document.createElement('li')
+// newli.innerText="MONGO_DB"
+// newone.replaceWith(newli)
+
+// const firstchild=document.querySelector('li:first-child')
+// firstchild.outerHTML="<li>typeScript</li>"
+
+// const removechild=document.querySelector('li:nth-child(2)')
+// removechild.remove()
+
+
+//console.log(document.title);
