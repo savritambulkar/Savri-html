b37c9f6f008af579850693459d6082edc5b73e1a
savritambulkar <savritambulkar4@gmail.com>
2024-12-30 06:22:27 -0800

    Add files via upload

---
+++ a12.java
@@ -0,0 +1,26 @@
+public class a12 {
+
+    public static void main(String[] args) {
+        
+        for(int p=1;p<=5;p++){
+              System.out.println(p);
+              if(p==5){
+                for(int i=1;i<10;i++){
+                    for(int j=5;j>0;j--){
+                       if(i==1||i-j==j){
+                      System.out.print(j);
+                    }
+                    else{
+                       System.out.print("");
+                    }
+                 }
+                }
+                System.out.println();
+                }
+                  }
+        for(int j=4;j>0;j--){
+            System.out.println(j);
+        }
+
+    }
+}
